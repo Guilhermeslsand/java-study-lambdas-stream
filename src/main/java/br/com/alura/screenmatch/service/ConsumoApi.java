0a9d387e5314d72faece6fcd6566c1218c5d689a
Guilherme Andrade <guilermeslandrade@gmail.com>
2026-07-09 14:58:14 -0300

    aula-01:Create service to read json from API

---
+++ src/main/java/br/com/alura/screenmatch/service/ConsumoApi.java
@@ -0,0 +1,29 @@
+package br.com.alura.screenmatch.service;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest;
+
+
+public class ConsumoApi {
+    public String obterDados(String endereco) {
+        // lógica para obter dados do endereço fornecido
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+    }
+}
